6f3c00fed0d3f9dabd2f17e32d32f815bf472109
hejohns <johnson.he2009@gmail.com>
2019-09-08 00:37:07 -0400

    modified wait call

--- foo.c
+++ foo.c
@@ -12,23 +12,41 @@ int main(int argc, char **argv)
 	{
 		foodprefix[i]=argv[1][i];
 	}
-	//start nginx as child
-	int forkreturn = fork();
-	if (forkreturn < 0)
+	//fork first child
+	int forkreturn1 = fork();
+	if (forkreturn1 < 0)
 	{
 		return 1;
 	}
-	else  if (forkreturn == 0)
+	else  if (forkreturn1 == 0)
 	{
+		//exec nginx
 		strcat(foodprefix, "/nginx/local/sbin/nginx");
 		char *path_to_sbin_nginx = foodprefix;
-		printf("%s\n", path_to_sbin_nginx);
 		execl(path_to_sbin_nginx, path_to_sbin_nginx, (char *)0);
 	}
 	else
 	{
-		wait(NULL);
-		return 0;
+		//fork second child
+		int forkreturn2 = fork();
+		if (forkreturn2 < 0)
+		{
+			return 1;
+		}
+		else if (forkreturn2 == 0)
+		{
+			for(int i=0; i<120; i++)
+			{
+				sleep(1);
+			}
+			return 0;
+		}
+		else
+		{
+			waitpid(forkreturn2, NULL, WNOHANG);
+			waitpid(forkreturn1, NULL, WNOHANG);
+			return 0;
+		}
 	}
 	
 }
