7d4200931c431b9b16dd9c91b2a46c9a2e2fd093
hejohns <johnson.he2009@gmail.com>
2019-09-27 23:53:40 -0400

    Added preliminary minecraft files

--- installer.sh
+++ installer.sh
@@ -42,3 +42,52 @@ WantedBy=multi-user.target' > food.service
 
 mv food.service ${systemd_unit_directory}food.service
 
+#wget minecraft server
+cd $foodprefix
+wget $minecraft_server_jar_uri
+java -Xms$minecraft_server_ram_min -Xmx$minecraft_server_ram_max -jar minecraft_server.$minecraft_server_version.jar nogui
+echo 'eula=true'>eula.txt
+echo 'broadcast-rcon-to-ops=true
+view-distance=10
+max-build-height=256
+server-ip=
+level-seed=
+rcon.port=25575
+gamemode=survival
+server-port=25565
+allow-nether=true
+enable-command-block=false
+enable-rcon=false
+enable-query=false
+op-permission-level=4
+prevent-proxy-connections=false
+generator-settings=
+resource-pack=
+level-name=world
+rcon.password=
+player-idle-timeout=0
+motd=food is running
+query.port=25565
+force-gamemode=false
+hardcore=false
+white-list=false
+broadcast-console-to-ops=true
+pvp=true
+spawn-npcs=true
+generate-structures=true
+spawn-animals=true
+snooper-enabled=true
+difficulty=hard
+function-permission-level=2
+network-compression-threshold=256
+level-type=amplified
+spawn-monsters=true
+max-tick-time=60000
+enforce-whitelist=false
+use-native-transport=true
+max-players=64
+resource-pack-sha1=
+spawn-protection=16
+online-mode=false
+allow-flight=false
+max-world-size=29999984'>server.properties
--- string_length.c
+++ string_length.c
@@ -3,32 +3,36 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct char_array{
 	int* pointer;
 	size_t size;
-}string;
+}dynamic_array;
 
-int main(){
-	string nginx_input;
-	nginx_input.pointer = malloc(1 * sizeof(int));
-	nginx_input.size = sizeof(int);
+int main(int argc, char **argv){
+	clock_t start = clock();
+	dynamic_array input;
+	input.pointer = malloc(1 * sizeof(int));
+	input.size = sizeof(int);
 	int c;
 	for (int i = 0; (c = fgetc(stdin)) != EOF; i++){
-		if ((int)(nginx_input.size < (int)((sizeof(int)*(i+1))))){
-			int *nginx_input_tmp = realloc(nginx_input.pointer, nginx_input.size+sizeof(int));
-			nginx_input.size += sizeof(int);
-			if (nginx_input_tmp == NULL){
+		if ((int)(input.size < (int)((sizeof(int)*(i+1))))){
+			int *input_tmp = realloc(input.pointer, input.size+sizeof(int));
+			input.size += sizeof(int);
+			if (input_tmp == NULL){
 				exit(1);
 			}
 			else{
-				nginx_input.pointer = nginx_input_tmp;
+				input.pointer = input_tmp;
 			}
 		}
-		nginx_input.pointer[i] = c;
-		//printf("%c", nginx_input.pointer[i]);
+		input.pointer[i] = c;
+		//printf("%c", input.pointer[i]);
 	}
-	printf("%d", (int)(nginx_input.size/sizeof(int))-1);
-	free(nginx_input.pointer);
+	printf("%d\n", (int)(input.size/sizeof(int))-1);
+	free(input.pointer);
+	clock_t end = clock();
+	printf("%lf\n", (double)(end-start)/CLOCKS_PER_SEC);
 	return 0;
 }
